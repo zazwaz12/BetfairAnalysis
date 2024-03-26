b468850ad4e1cf74899b70507810e405ef1b8ce2
Tim <timothyszandi@gmail.com>
2024-03-26 21:38:53 +0800

    Placing bets after testing betfairlightweight stream, same issue persists: The issue is that the market filter does not select only the relevant markets and blows out to over 200.

--- session.py
+++ session.py
@@ -66,4 +66,47 @@ session = loggedIn.get_session()
 logger.info(f"new session attempt: {loggedIn.get_status()}")
 loggedIn.update_environment_variables(".env")
 
+import betfairlightweight
 
+trading = betfairlightweight.APIClient(loggedIn.username, loggedIn.password, os.getenv("APPLICATION_KEY"))
+
+trading.login_interactive()
+event_types = trading.betting.list_event_types()
+
+markets = trading.betting.list_market_catalogue(filter={"event_type_id":"4"})
+markets = [m.market_id for m in markets]
+print(markets)
+info = trading.betting.list_market_book(markets)
+
+selectionID = [(x.selection_id) for x in [book.runners for book in info][0]][0]
+instruction = [
+        {
+            "selectionId": f"{selectionID}",
+            "side": "BACK",
+            "orderType": "LIMIT",
+            "limitOrder": {
+                "size": "10",
+                "price": "2",
+                "persistenceType": "PERSIST"
+            }
+        }
+    ]
+print(markets[0], instruction)
+
+trading.betting.place_orders(markets[0], instruction)
+#print([o.selection_id for o in trading.betting.list_current_orders()])
+#print([i.event for i in trading.betting.list_events()])
+'''"instructions": [
+                {
+                    "selectionId": "",
+                    "side": "BACK",
+                    "orderType": "LIMIT",
+                    "limitOrder": {
+                        "size": "10",
+                        "price": "2",
+                        "persistenceType": "PERSIST"
+                    }
+                }
+            ]
+
+'''
--- stream.py
+++ stream.py
@@ -5,6 +5,18 @@ import logging
 from dotenv import load_dotenv 
 from session import BetfairLogin
 
+from datetime import datetime, timedelta
+
+# Get the current time
+current_time = datetime.now()
+
+# Add 12 hours to the current time
+end_time = current_time + timedelta(hours=1)
+
+# Format the timestamps in the required format (ISO 8601)
+current_time_str = current_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+end_time_str = end_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
 # Set up logging
 log_format = '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s'
 logging.basicConfig(filename='betfair.log', level=logging.INFO, format=log_format, datefmt='%d-%b %H:%M')
@@ -27,6 +39,7 @@ options = {
     'port': 443
 }
 
+count = 0
 # Establish connection to the socket
 context = ssl.create_default_context()
 with socket.create_connection((options['host'], options['port'])) as sock:
@@ -39,7 +52,10 @@ with socket.create_connection((options['host'], options['port'])) as sock:
         
         # Subscribe to order/market stream
         # event id for cricket, tennis and AFL are: 2, 4 and 61420
-        market_subscription_message = '{"op":"marketSubscription","marketFilter":{"bettingTypes":["ODDS"],"eventTypeIds":["4", "61420"],"turnInPlayEnabled":true,"marketTypes":["MATCH_ODDS"]},"marketDataFilter":{}}\r\n'
+        market_subscription_message = '{"op":"marketSubscription","marketFilter":{"eventTypeIds":["2"],"marketTypes":["MATCH_ODDS"]},"marketDataFilter":{"ladderLevels": 1, "fields":["EX_BEST_OFFERS", "SP_TRADED"]}}\r\n'
+        #market_subscription_message = market_subscription_message.replace("current_time_str", current_time_str)
+        #market_subscription_message = market_subscription_message.replace("end_time_str", end_time_str)
+        print(market_subscription_message)
         ssock.sendall(market_subscription_message.encode())
         
         # Receive data from the socket
@@ -47,6 +63,7 @@ with socket.create_connection((options['host'], options['port'])) as sock:
             data = ssock.recv(1024)
             if not data:
                 break
-            print("Received:", data.decode())
+            count += 1
+            print(f"Received {count}:", data.decode())
 
 print('Connection closed')
